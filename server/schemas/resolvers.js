cd6c3b818ef46987edd5b8ed45f4c6e147b459fd
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-10-10 14:35:58 -0400

    updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,7 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
 
 const resolvers = {
     Query: {
@@ -7,7 +10,35 @@ const resolvers = {
         }
     },
     Mutation: {
-        add
+        addUser: async (parent, { username, email, password}) => {
+            const user = await User.create({username, email, password});
+            const token = signToken(user);
+
+            return { token, user};
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+    
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+
+            return { token, user };
+          },
+          saveBook: async (parent, args) => {
+            return Book.create(args);
+          },
+          removeBook: async(parent, {bookId}) => {
+              return Book.findOneAndDelete({_id: bookId});
+          }
     }
 }
 
